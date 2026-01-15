29800617f0de66fccc35e057c78c5e7d9a6db433
sabbir990 <muhammadsabbirkhanalif@gmail.com>
2026-01-15 18:12:07 +0600

    Connected successfully to MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import {Server} from "http"
 import app from "./app";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 let server: Server;
 
 const PORT = 3000;
+const mongoDB_connection_string: string = process.env.MONGODB_CONNECTION_STRING as string;
 
 export default function main(){
     try{
+        const connection = mongoose.connect(mongoDB_connection_string);
+
+        console.log("Mongoose is connected!");
+        
         server = app.listen(PORT, () => {
             console.log("This server is running on port ", PORT);
         })
